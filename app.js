bc5c10cd39e972cd60448bc30726b71cd8799214
kotekirkitadze <kotekirkitadze@gmail.com>
2021-12-13 17:52:57 +0400

    in progress routes and schemas

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 require("dotenv/config");
 
 const api = process.env.API_URL;
+const productsRoute = require("./routers/products");
 
 //MiddleWare
 //ამ მიდლევეარით ვაანალიზებთ ფრონტენდიდან წამოსლ მონაცემებს, ვპარსავთ
@@ -14,20 +16,13 @@ app.use(express.json());
 //to log http requests on the console
 app.use(morgan("tiny"));
 
-app.get(`${api}/products`, (req, res) => {
-	const product = {
-		id: 1,
-		name: "My haircut staff",
-		image: "Some url",
-	};
-	res.send(product);
-});
+//Routers
+app.use(`${api}/products`, productsRoute);
 
-app.post(`${api}/products`, (req, res) => {
-	const newProduct = req.body;
-	console.log(newProduct);
-	res.send(newProduct);
-});
+mongoose
+	.connect(process.env.CONNECTION_STRING)
+	.then(() => console.log("Database Connection is ready..."))
+	.catch((err) => console.log(err));
 
 app.listen(3000, () => {
 	console.log("Server started on port: http://localhost:3000/");
