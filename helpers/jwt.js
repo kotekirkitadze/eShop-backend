ce52c0d7df20a8c8d739277e8acf276f7ef0b22a
kotekirkitadze <kotekirkitadze@gmail.com>
2021-12-27 01:04:03 +0400

    minor change regarding auth

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -11,7 +11,7 @@ function authJwt() {
 		path: [
 			{ url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
 			{ url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
-			{ url: /\/api\/v1\/category(.*)/, methods: ["GET", "OPTIONS"] },
+			{ url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
 			`${api}/users/login`,
 			`${api}/users/register`,
 			// { url: /(.*)/ },
