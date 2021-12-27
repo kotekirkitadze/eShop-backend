e08ea53c8a08b3451b856e4d83a6d6b3c37598af
kotekirkitadze <kotekirkitadze@gmail.com>
2021-12-27 13:21:46 +0400

    minor updates

--- routers/orders.js
+++ routers/orders.js
@@ -121,8 +121,8 @@ router.post("/create-checkout-session", async (req, res) => {
 		payment_method_types: ["card"],
 		line_items: lineItems,
 		mode: "payment",
-		success_url: "http://localhost:4200/success",
-		cancel_url: "http://localhost:4200/products",
+		success_url: "https://kotekirkitadze.github.io/ngshop/success",
+		cancel_url: "https://kotekirkitadze.github.io/ngshop/products",
 	});
 
 	res.json({ id: session.id });
